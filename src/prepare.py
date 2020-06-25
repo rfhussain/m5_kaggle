21dcc1bdae5e34560e2a1365e8ebbb5234a255d0
rfhussain <imraheelhussain@gmail.com>
2020-06-25 20:10:58 +0300

    updated utils.py and prepare.py

---
+++ src/prepare.py
@@ -0,0 +1,138 @@
+import utils
+import pandas as pd
+import numpy as np
+import os
+import gc
+
+class M5AccuracyCook():
+    def __init__(self, submission_path, days_to_train):
+        super().__init__()
+        self.submission_path = submission_path
+        self.__data_folder = '..//data//'
+        self.__m5util = utils.M5AccuracyUtils(self.submission_path)
+        self.__days_to_train = days_to_train
+        
+    
+    def cook_data(self):
+        '''
+        This is a data preparation class method
+        This method will cook the data for us and create multipe feature sets, to train the data
+        '''
+        #######################################################
+        # Data Preparation Pipeline
+        #
+        #
+        # Remember, that scikit-learn also provides a library
+        # for pipeline, where we can add even the training of 
+        # model in the pipeline
+        #######################################################
+
+
+        # 1. read the evaluation data set
+        df_eval = pd.read_csv(os.path.join(self.__data_folder,'sales_train_evaluation.csv'))
+
+        # 2. melting the data frame
+        df_eval = self.__m5util.melt_dataframe(df_eval)
+
+        # 3. joining the calendar and prices
+        df_eval = self.__m5util.merge_sales_calendar_prices(df_eval)
+
+        # 4. adding the group by columns
+        group_by_cols = ['item_id','dept_id']
+        df_eval = self.__m5util.add_group_by_cols(group_by_cols,df_eval)
+
+        # 5. adding the label encodes
+        df_eval = self.__m5util.add_label_encodes(df_eval)
+
+        # 6. adding the Means
+        mean_enc_cols = ['store_id_code','cat_id_code','state_id_code','item_id_code','dept_id_code']
+        index_cols = ['item_id_code','store_id_code']
+        df_eval = self.__m5util.get_mean_attributes(mean_enc_cols,index_cols,df_eval)
+
+        #########################################################
+        # IMPORTANT: for time being we are just getting 180 days
+        # but will have to get at least 1 year of data
+        #########################################################
+
+        # 7. getting last 180 days of data
+        df_eval =df_eval[(df_eval.d > (df_eval.d.max() - self.__days_to_train))]
+
+
+        # 8. adding the lags
+        mean_enc_cols = [col for col in df_eval.columns if 'mean' in str(col)]
+        exception_cols = mean_enc_cols + ['cat_id', 'date', 'day', 'id','sell_price', 'snap_CA', 'snap_TX', 'snap_WI',
+                                  'state_id', 'wday', 'wm_yr_wk','year','month','dom']
+
+        index_cols = ['store_id_code','item_id_code','d']
+        shift_range = [x for x in range(1,10)] 
+
+        df_eval = self.__m5util.add_lags(df_eval,shift_range,index_cols,exception_cols)
+
+
+        #########################################################
+        # 9. Train Test Splits
+        # The split will be performed and results will be saved
+        # The training will be performed in the train.py
+        # 
+        # 
+        # Note: currently we are creating only one feature set
+        # but future we will be creating multiple feature set
+        #########################################################
+
+        drop_columns= ['id','target','target_dept','target_item','month','year','dom']
+
+        # ########## TRAINING DATA (This data will be used to predict for X_valid)
+        X_train = df_eval[df_eval.d <= df_eval.d.max()-28].drop(drop_columns, axis=1) 
+        y_train = df_eval[df_eval.d <= df_eval.d.max()-28]['target']
+
+        # ########## TRAINING DATA (This data will be used to predict for X_Test) 
+        # so without any filtering, we take complete evaluation set, 
+        # because the prediction is in the future and data isn't available        
+        X_train_final = df_train.drop(drop_columns, axis=1) 
+        y_train_final = df_train['target']
+
+        # ########## VALIDATION DATA (for prediction from 1913 ~ 1941) 
+        X_valid = df_eval[df_eval.d > df_eval.d.max()-28].drop(list(set(drop_columns) - set(['id'])), axis=1)
+        y_valid = df_eval[df_eval.d > df_eval.d.max()-28]['target']
+        # convert the id from _evauation to _validation
+        X_valid['id'] = X_valid['id'].apply(lambda x: x.replace('_evaluation','_validation'))
+
+        # ########## EVALUATION DATA  (for prediction from 1942 ~ 1969) 
+        X_test  =  df_eval[df_eval.d > df_eval.d.max()-28].drop(list(set(drop_columns) - set(['id'])), axis=1)
+        #adding 28 for all the days
+        X_test.d = X_test.d + 28 
+
+
+        # 10. Saving the Records
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,222 @@
-DATA_FOLDER = '..//data//'
+import pandas as pd
+import numpy as np
+import gc
+import os
+from tqdm import tqdm_notebook
+from datetime import datetime
+from sklearn.preprocessing import LabelEncoder
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+
+class M5AccuracyUtils():
+
+    def __init__(self, submission_path):
+        super().__init__()
+        self.__data_folder = '..//data//'
+        self.__submission_path = submission_path
+
+
+    def get_mean_attributes(self, mean_cols,index_cols, df):
+        '''
+        This function will add the required mean attributes
+        Parameters:
+        - mean_cols : list like ['cols_1','col_2',.....]
+        - index_cols: list .. for this dataframe ['item_id_code','item_id_code']
+        - df : mean data frame to add mean to        
+        '''
+        for col in mean_cols:
+            mean_attrib = col + '_target_mean'
+            mean_values = df.groupby(col).target.mean()
+            df[mean_attrib] = df[col].map(mean_values)
+            # deleting the columns after adding the mean
+            if col not in index_cols:
+                df.drop([col], axis=1, inplace=True)
+            #type casting
+            df[mean_attrib] = df[mean_attrib].astype('float16')
+        
+        gc.collect()
+        # returning the resulting df
+        return df
+
+    def del_additional_cols(self,df):
+        '''
+        This function is temporary
+        Remember, that for basic data for training, we are deleting some wanted colummns, which are not to be deleted
+        '''
+        df = df.drop(['wm_yr_wk','wday','sell_price'], axis=1)
+        return df
+
+    def _get_d_of_m(self,col): 
+        return col.day
+
+    def _get_calendar_df(self):
+        '''
+        - This function will return the calendar data frame 
+        - It will also remove d_ from the day column
+        - and type cast it to int
+        '''
+        d_parser = lambda x: pd.datetime.strptime(x,'%Y-%m-%d')
+        df = pd.read_csv(os.path.join(self.__data_folder,'calendar.csv'), parse_dates=["date"], date_parser=d_parser)
+        df['d'] = df['d'].apply(lambda x: x.replace('d_',''))
+        df['d'] = df['d'].astype('int16')
+        gc.collect()
+
+        #get day of month
+        df['dom'] = df['date'].apply(self._get_d_of_m)
+        #returning the data frame
+        return df
+
+
+    def _get_prices_df(self):
+        df = pd.read_csv(os.path.join(self.__data_folder,'sell_prices.csv'))
+        return df
+
+    def merge_sales_calendar_prices(self,df):
+        df_cal = self._get_calendar_df()
+        df_prices = self._get_prices_df()
+
+        #merging sales with calendar
+        df = df.merge(df_cal[['date','wm_yr_wk','wday','d','month','year','dom']], on=['d'], how='left')
+        
+        #merging sales with prices
+        df = df.merge(df_prices, how='left', on=['store_id','item_id','wm_yr_wk'])
+
+        #type casting the resulting columns
+        df.wm_yr_wk     = df.wm_yr_wk.astype('int16')
+        df.wday         = df.wday.astype('int8')
+        df.month        = df.month.astype('int8')
+        df.dom          = df.dom.astype('int8')
+        df.year         = df.year.astype('int16')
+        df.sell_price   = df.sell_price.astype('float16')
+
+        #deleting the dataframes not required
+        del df_cal, df_prices
+        gc.collect()
+
+        #returning the dataframe
+        return df
+
+    def melt_dataframe(self,df):
+        
+        # we will only work on the evaluation dataset
+        df_rows = df.melt(
+        id_vars=['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'],
+        var_name ='d',
+        value_name ='target'
+        )
+
+        #remove the d_
+        df_rows['d']   = df_rows['d'].apply(lambda x: x.replace('d_',''))
+
+        #down casting
+        df_rows.d        = df_rows.d.astype('int16')
+        df_rows.target   = df_rows.target.astype('int16')
+
+        gc.collect()
+
+        return df_rows
+
+    def add_group_by_cols(self,cols,df):
+        '''
+        This function will add the group by columns to the df and return the resulting df
+        Parameters:
+        - cols : a list of columns ['col-1','col-2',...]
+        - df   : the main data frame where to add the group by        
+        '''
+        for col in cols:
+            gb = df.groupby([col,'d'], as_index=False).target.sum()
+            gb.rename(columns = {'target','target_' + str(col)})
+            df = df.merge(gb, how='left', on=[col,'d']).fillna(0)
+            del gb
+            gc.collect()
+        #returning the data frame
+        return df
+
+    def add_label_encodes(self,df):
+        '''
+        This function will add the label encoders on the following columns
+        dept_id, cat_id, store_id, state_id, item_id
+
+        It will delete the above columns since the label encode has been added
+        '''
+        #adding the label encoding
+        le = LabelEncoder()
+        df['dept_id_code'] = le.fit_transform(df.dept_id)
+        df['cat_id_code'] = le.fit_transform(df.cat_id)
+        df['store_id_code'] = le.fit_transform(df.store_id)
+        df['state_id_code'] = le.fit_transform(df.state_id)
+        df['item_id_code'] = le.fit_transform(df.item_id)
+
+        #deleting the columns
+        df.drop(['dept_id','cat_id','store_id','state_id','item_id'], axis=1, inplace=True)
+
+        #type casting to save memory
+        df.dept_id_code = df.dept_id_code.astype('int8')
+        df.cat_id_code  = df.cat_id_code.astype('int8')
+        df.store_id_code= df.store_id_code.astype('int8')
+        df.state_id_code= df.state_id_code.astype('int8')
+        df.item_id_code = df.item_id_code.astype('int16')
+
+        #returning the df
+        return df
+
+    def add_lags(self, df, shift_range, index_cols, exception_cols):
+        '''
+        This function will add the lags to the data frame
+
+        Parameters: 
+        - df : the data frame to add lags to
+        - shift_range : list of how many lags like [1,2,3,...]
+        - index_cols : list of index cols in our case ['item_id_code','store_id_code']
+        - exception_cols : list of those columns which are not supposed to be lags ['col1','col2',....]
+        '''
+
+        #getting the lag columns
+        cols_to_rename = list(df.columns.difference(index_cols + exception_cols)) 
+
+        for day_shift in tqdm_notebook(shift_range):
+
+            train_shift = df[index_cols + cols_to_rename].copy()
+            print('copied to train_shift...')
+
+            train_shift['d'] = train_shift['d'] + day_shift
+            print(f'added lag by adding {day_shift} to [d] column...')
+            
+            foo = lambda x: '{}_lag_{}'.format(x, day_shift) if x in cols_to_rename else x
+            train_shift = train_shift.rename(columns=foo)
+            print('renamed the columns as col_lag_x...')
+
+            df = pd.merge(df, train_shift, on=index_cols, how='left').fillna(0)
+            print(f'performed the merge ---------{day_shift}--------------..')
+
+        
+        print('\n\n DONE\n\n')
+
+        # type casting the lag columns (in my opinion lags are int values)
+        for lag_col in [col for col in df.columns if 'lag' in str(col)]:
+            df[lag_col] = df[lag_col].astype('int16')  
+        print('type casted all lag columns to int16 (please note: i did int because sample submission file has int values for results)...')
+
+        # returning the resulting data frame
+        return df
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
